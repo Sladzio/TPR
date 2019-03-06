7313b032558d213db12570d36df4a5b4cf24c216
Sladzio <sladzio995@gmail.com>
2019-03-06 16:07:09 +0100

    *Added implementation of testing bandwidth and latency

---
+++ lab1/main.py
@@ -0,0 +1,59 @@
+from mpi4py import MPI
+import numpy as np
+import json
+import sys
+
+MBIT_FACTOR = 8.0 / (1024 * 1024)
+
+result = {}
+max_payload_size = 10 ** 6
+iterations = 1000
+comm = MPI.COMM_WORLD
+rank = world.Get_rank()
+
+
+def run():
+    if len(sys.argv) < 2:
+        print "Provide output json file name"
+    test_bandwidth_and_latency("send", lambda x: comm.send(x, dest=1))
+    test_bandwidth_and_latency("ssend", lambda x: comm.ssend(x, dest=1))
+    if rank == 0:
+        print_to_json()
+
+
+def print_to_json():
+    with open(sys.argv[1], 'w') as fp:
+        json.dump(sample, fp)
+
+
+def test_bandwidth_and_latency(comm_type, send):
+    test_result = {"latency": [], "bandwidth": [], "payload_size": []}
+    for payload_size in xrange(max_payload_size):
+        payload = np.random.bytes(payload_size)
+        comm.Barrier()
+        t1 = MPI.Wtime()
+        for _ in xrange(iterations):
+            if rank == 0:
+                send(payload)
+            elif rank == 1:
+                recv()
+        t2 = MPI.Wtime()
+
+        latency = convert_to_milliseconds((t2 - t1) / iterations)
+        test_result["latency"].append(latency)
+        test_result["payload_size"].append(payload_size)
+        test_result["bandwidth"] = MBIT_FACTOR * (payload_size * iterations) / t2 - t1
+
+    result[comm_type] = test_result
+
+
+def convert_to_milliseconds(seconds):
+    return seconds * 1000
+
+
+def recv():
+    comm.recv(source=0)
+
+
+if __name__ == "__main__":
+    run()
