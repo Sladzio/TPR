dce9c25e1e3d16a34e5ca1b553a5385aca369281
Patryk Sladek <sladzio995@gmail.com>
2019-03-11 15:41:07 +0100

    *Testing scatter impl

--- lab2/mapReduce.py
+++ lab2/mapReduce.py
@@ -7,6 +7,7 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 a_size = 3
+
 recvdata = numpy.zeros(a_size, dtype=numpy.int)
 senddata = (rank + 1) * numpy.arange(a_size, dtype=numpy.int)
 comm.Reduce(senddata, recvdata, root=0, op=MPI.SUM)
