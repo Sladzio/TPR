9ffb261eb550d8e2a90681006db5d742c8e82420
Patryk Sladek <sladzio995@gmail.com>
2019-05-02 09:09:38 +0200

    *Added charts

--- lab4/run.sh
+++ lab4/run.sh
@@ -7,9 +7,9 @@ printf "\n" >> results_scalable.txt
 printf "time,number_of_points,number_of_buckets,range_of_numbers,number_of_threads" >> results_non-scalable.txt
 printf "\n" >> results_non-scalable.txt
 
-for NUMBER_OF_POINTS in 10000000 100000000
+for NUMBER_OF_POINTS in 1000000 10000000
 do
-    for NUMBER_OF_BUCKETS in 100 1000
+    for NUMBER_OF_BUCKETS in 4 20 40
     do
         for RANGE_OF_NUMBERS in 100 1000 10000
         do
